257a30caf33955438d39e40ef92bc7febaa71567
Charlie Doherty <charlieedoherty@gmail.com>
2024-02-02 00:28:06 -0600

    Add documentation

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import streamlit as st
 import pandas
 
+# Keeps the page from being too narrowly restricted
 st.set_page_config(layout="wide")
 
+# Text displayed at the top of the page
 st.title("Welcome to The Company!")
 content = ("Here at The Company, innovation meets excellence in every endeavor. As a pioneering force in our industry, "
            "we are dedicated to delivering unparalleled solutions that redefine standards and exceed expectations. "
@@ -11,19 +13,22 @@ content = ("Here at The Company, innovation meets excellence in every endeavor. 
            "we harness the latest technologies and strategic insights to drive transformative outcomes. Explore our "
            "comprehensive suite of services and discover how we can catalyze your success today.")
 st.write(content)
-st.markdown("***")
+st.markdown("***")  # Break line
 st.title("Meet the team")
 
-col1, col2, col3, col4 = st.columns(4)
 df = pandas.read_csv("data.csv")
+col1, col2, col3, col4 = st.columns(4)
 
 with col1:
+    # index only created as iterrows enumerates
+    # Divide the rows of the csv file into 4 displayed columns with employees names, roles, and pictures
     for index, row in df[:3].iterrows():
         st.subheader(row["first name"].capitalize() + " " + row["last name"].capitalize())
         st.write(row["role"])
         st.image("images\\" + row["image"])
         st.title("")
 
+# Repeat for every row
 with col2:
     for index, row in df[3:6].iterrows():
         st.subheader(row["first name"].capitalize() + " " + row["last name"].capitalize())
